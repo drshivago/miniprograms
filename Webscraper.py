a0fdae5404386ab96ecd4488c7e3128f5e7a01ed
drshivago <moessner2015@gmail.com>
2023-04-03 18:03:57 -0400

    fixed filename issue

--- Webscraper.py
+++ Webscraper.py
@@ -3,13 +3,14 @@ from bs4 import BeautifulSoup
 import shutil
 
 link = "https://worldcosplay.net/photo/6799292"
-name = "image"
+#name = "image"
+#add json reading bit here
 r = requests.get(link)
 soup = BeautifulSoup(r.content, features="html.parser")
 sauce = soup.find(property="og:image")["content"]
-#print(sauce)
+print(sauce)
 res = requests.get(sauce, stream = True)
 
 if res.status_code == 200:
-    with open(name + '.jpg', 'wb') as f:
+    with open(sauce[36:-4] + '.jpg', 'wb') as f:
         shutil.copyfileobj(res.raw, f)
