3f808c5458b4ee7e64fdd6274f21deafe2560ec4
drshivago <moessner2015@gmail.com>
2023-04-06 18:45:03 -0400

    shifts thu photo tags

---
+++ webfinder.py
@@ -0,0 +1,12 @@
+import requests
+import random
+from bs4 import BeautifulSoup
+import webbrowser
+import shutil
+
+link = "https://worldcosplay.net/tag/"
+random.seed
+for x in range(900, 1000):
+
+    sauce = link + str(x)
+    webbrowser.open_new_tab(sauce)
